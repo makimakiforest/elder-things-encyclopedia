078584f8d86f4063613c21a6ed2b48a04bd7bd3d
FooHogeFoo <keiyosida1015@gmail.com>
2024-06-02 16:25:43 +0900

    wip

--- db/init.sql
+++ db/init.sql
@@ -3,3 +3,21 @@ DROP TABLE IF EXISTS collection;
 CREATE TABLE collection (id SERIAL PRIMARY KEY, title TEXT);
 INSERT INTO collection (title) values('test1');
 INSERT INTO collection (title) values('test2');
+
+CREATE TABLE CHARACTER 
+(
+     id UUID PRIMARY KEY,
+     name TEXT 
+);
+
+CREATE TABLE profile  
+(
+     id PRIMARY KEY
+
+);
+
+CREATE TABLE MAPPING -- 
+(
+    character_id for,
+    profile_id 
+)
