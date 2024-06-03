d7cca379bd4bf5cdad33eef0c653cc3c842516b2
FooHogeFoo <keiyosida1015@gmail.com>
2024-06-03 22:10:43 +0900

    add: schemas and tables to init sql

--- db/init.sql
+++ db/init.sql
@@ -1,23 +1,49 @@
-DROP TABLE IF EXISTS collection;
+DROP SCHEMA IF EXISTS dev;
+DROP SCHEMA IF EXISTS tst;
+DROP TABLE IF EXISTS dev.character;
+DROP TABLE IF EXISTS dev.profile;
+DROP TABLE IF EXISTS dev.character_profile;
+DROP TABLE IF EXISTS tst.character;
+DROP TABLE IF EXISTS tst.profile;
+DROP TABLE IF EXISTS tst.character_profile;
 
-CREATE TABLE collection (id SERIAL PRIMARY KEY, title TEXT);
-INSERT INTO collection (title) values('test1');
-INSERT INTO collection (title) values('test2');
+-- スキーマの作成
+CREATE SCHEMA IF NOT EXISTS dev;
+CREATE SCHEMA IF NOT EXISTS tst;
 
-CREATE TABLE CHARACTER 
+-- uuid-osspのインストール
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- テーブル作成 dev
+CREATE TABLE IF NOT EXISTS dev.character
 (
-     id UUID PRIMARY KEY,
-     name TEXT 
+     character_id UUID PRIMARY KEY,
+     name         VARCHAR
+);
+CREATE TABLE IF NOT EXISTS dev.profile
+(
+    profile_id UUID PRIMARY KEY
 );
 
-CREATE TABLE profile  
+CREATE TABLE IF NOT EXISTS dev.character_profile
 (
-     id PRIMARY KEY
-
+    character_id UUID REFERENCES dev.character (character_id) ON DELETE CASCADE,
+    profile_id UUID   REFERENCES dev.profile (profile_id)     ON DELETE SET NULL
 );
 
-CREATE TABLE MAPPING -- 
+-- テーブル作成 tst
+CREATE TABLE IF NOT EXISTS tst.character
 (
-    character_id for,
-    profile_id 
-)
+     character_id UUID PRIMARY KEY,
+     name         VARCHAR
+);
+CREATE TABLE IF NOT EXISTS tst.profile
+(
+    profile_id UUID PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS tst.character_profile
+(
+    character_id UUID REFERENCES tst.character (character_id) ON DELETE CASCADE,
+    profile_id   UUID REFERENCES tst.profile (profile_id)     ON DELETE SET NULL
+);
