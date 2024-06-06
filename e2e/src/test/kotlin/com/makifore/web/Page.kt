4fa03994655a233deda493d894762aff8bb6182f
kamegoro <tentyo0620@icloud.com>
2024-06-06 22:55:44 +0900

    add: e2e tests

--- e2e/src/test/kotlin/com/makifore/web/Page.kt
+++ e2e/src/test/kotlin/com/makifore/web/Page.kt
@@ -1,10 +1,22 @@
 package com.makifore.web
 
 import com.thoughtworks.gauge.Step
+import com.codeborne.selenide.Condition.*
+import com.codeborne.selenide.Selenide.*
 
-class Header {
-    @Step("ヘッダーに<title>と表示されている")
+class Page {
+    @Step("キャラクター一覧画面を開く")
+    fun openCharacterPage() {
+        open("http://localhost:10070/")
+    }
+
+    @Step("キャラクター一覧画面が表示されること")
+    fun displayCharacterPage() {
+        `$`(".character-page").shouldBe(exist)
+    }
+
+    @Step("ヘッダーに<title>と表示されること")
     fun displayHeaderTitle (title: String) {
-        
+        `$`(".header-title").shouldHave(exactText(title))
     }
 }
