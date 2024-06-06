390366ac78afe34d840171556ce67a5dbb6ae953
kamegoro <tentyo0620@icloud.com>
2024-06-06 22:54:46 +0900

    add: e2e new files

---
+++ e2e/specs/web/page.spec
@@ -0,0 +1,6 @@
+# Header
+* キャラクター一覧画面を開く
+* キャラクター一覧画面が表示されること
+
+## Headerが表示されている
+* ヘッダーに"elder-things-encyclopedia"と表示されること
---
+++ e2e/src/test/kotlin/com/makifore/web/Page.kt
@@ -0,0 +1,10 @@
+package com.makifore.web
+
+import com.thoughtworks.gauge.Step
+
+class Header {
+    @Step("ヘッダーに<title>と表示されている")
+    fun displayHeaderTitle (title: String) {
+        
+    }
+}
