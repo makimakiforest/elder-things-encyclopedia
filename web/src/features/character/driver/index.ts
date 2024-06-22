feb0bcbad44dd03f474c14120f1615cc0286909c
kamegoro <tentyo0620@icloud.com>
2024-06-23 08:59:08 +0900

    add: character driver

--- web/src/features/character/driver/index.ts
+++ web/src/features/character/driver/index.ts
@@ -1,3 +1,14 @@
+'use client';
+
+import { GetCharacters200 } from '@/types/openapi/schemas';
+
 interface CharacterDriver {
-  getCharacters: () => Promise<void>;
+  getCharacters: () => Promise<GetCharacters200>;
+}
+
+export class CharacterDriverImpl implements CharacterDriver {
+  async getCharacters(): Promise<GetCharacters200> {
+    const response = await fetch('/characters');
+    return response.json();
+  }
 }
